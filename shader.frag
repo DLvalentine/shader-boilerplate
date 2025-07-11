fdc6c1cae5a76c960200fdc3fcc9d1a991046043
David L Valentine <davidlewisvalentine@gmail.com>
2025-07-10 20:26:27 -0700

    Add files via upload

--- shader.frag
+++ shader.frag
@@ -1,5 +1,7 @@
 precision mediump float; // setting the precision
 
+#include "color/desaturate.glsl" // example include from a library like lygia.
+
 // uniforms set from application code
 uniform vec2 u_mouse; // mouse x/y pos
 uniform float u_time; // time since starting the drawing
@@ -9,5 +11,5 @@ varying vec2 pos; // represents screen space pixel location, normalized from 0-1
 // This makes a fun color gradient using some uniforms passed from P5 and a varying from our vert shader
 void main() {
   vec3 color = vec3(u_mouse.x, pos.y, u_time * 0.75);
-  gl_FragColor = vec4(abs(sin(color)), 1.0); // "output", what the frag color should be
+  gl_FragColor = vec4(abs(sin(desaturate(color, 0.85))), 1.0); // "output", what the frag color should be
 }
--- sketch.js
+++ sketch.js
@@ -1,7 +1,33 @@
 let s;
+let vertShader;
+let fragShader;
 
-function preload() {
-  s = loadShader('shader.vert', 'shader.frag');
+// Quick and dirty way to inject lygia functions. It currently can't inject function dependencies (e.g. if a space func depends on a math func), so you'll just need to include the dependencies yourself nbd
+// NOTE: You need to declare precision BEFORE your includes.
+// NOTE: lygia should exist outside of this directory, in its own dir. Be sure to change base if you move it
+// NOTE: You must include the extension ex. #include "color/desaturate.glsl"
+async function loadShaderWithIncludes(path, base = './', ignoreDeps = false){
+  const code = await fetch(path).then(res => res.text());
+  const lines = code.split('\n');
+  const processed = await Promise.all(lines.map(async line => {
+    const match = line.match(/#include\s+"(.+?)"/);
+    if (match && !ignoreDeps) {
+      const includePath = base + match[1];
+      return await loadShaderWithIncludes(includePath, base, true);
+    } else if (match && ignoreDeps){
+      return '';
+    } else {
+      return line;
+    }
+  }));
+  return processed.join('\n');
+};
+
+
+async function preload() {
+  vertShader = await loadShaderWithIncludes('shader.vert', '../lygia/');
+  fragShader = await loadShaderWithIncludes('shader.frag', '../lygia/');
+  s = await new p5.Shader(this._renderer, vertShader, fragShader);
 };
 
 function setup() {
